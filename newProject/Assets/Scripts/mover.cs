1850a94e485abd62b2dffd208e99483f8f44f8d0
Luka Locniskar <luka.locniskar@gmail.com>
2014-11-30 02:24:00 +0100

    dodal 'newProject'

---
+++ newProject/Assets/Scripts/mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour {
+	public int orientation;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		Vector3 vecX = new Vector3 (orientation, 0, 0);
+		rigidbody.velocity = vecX * speed;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
