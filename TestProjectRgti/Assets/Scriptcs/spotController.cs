3e6e1055cb11d01ae4b977dcd440c644c0cda874
Luka Locniskar <luka.locniskar@gmail.com>
2014-12-22 10:52:54 +0100

    testni projekt kinect

---
+++ TestProjectRgti/Assets/Scriptcs/spotController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spotController : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+		gameObject.light.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			gameObject.light.enabled= !gameObject.light.enabled;
+		}
+	}
+}
