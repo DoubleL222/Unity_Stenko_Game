3f8982c8d1a13f0a6d5b07fc1f64eab58f55fcbc
Luka Locniskar <luka.locniskar@gmail.com>
2015-01-10 19:39:07 +0100

    forgot to add

--- Test2/Assets/Scripts/CharacterMove.cs
+++ Test2/Assets/Scripts/CharacterMove.cs
@@ -2,15 +2,49 @@
 // Do test the code! You usually need to change a few small bits.
 
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CharacterMove : MonoBehaviour {
 
+	public Slider slider;
+	public float gravityK=0.1f;
+//	public float flashSpeed =5f;
+//	public Color flashColour = new Color (1f, 0f, 0f, 0.1f);
+	public float maxFuel = 100f;
+	public float speed =6f;
+	public float speedVertical =6f;
+	Vector3 movement;
+	Rigidbody playerRigidbody;
 	private float nextJump;
+	private float fuel;
+	float camRayLength=100f;
+
+	void Awake()
+	{
+		fuel = maxFuel;
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
 
 	void  FixedUpdate (){
-		MoveHero();
-		JumpHero();
+
+		rigidbody.AddForce(-Vector3.up*9.8f*gravityK);
+		float v=Input.GetAxisRaw("Vertical");
+		float h=-Input.GetAxisRaw("Horizontal");
+		fuel = fuel -  Mathf.Abs(v);
+		if (fuel < 0) {
+			v=0;	
+			}
+		if (v == 0 && fuel<=maxFuel ) {
+				fuel += maxFuel * 0.01f;
+		}
+		MoveHero(h,v);
+		print ("fuel: " + fuel);
+			//	JumpHero();
+		slider.value = fuel;
+	}
+
+	void Update(){
 	}
 	
 	void  PlayAnimation ( string AnimName  ){
@@ -23,7 +57,7 @@ public class CharacterMove : MonoBehaviour {
 		if (!animation.isPlaying) animation.Play("idle");
 	}
 	
-	void  MoveHero (){
+/*	void  MoveHero (){
 		if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f) {
 			if (Input.GetAxis("Horizontal") > 0.02f) transform.eulerAngles = new Vector3(0f, 90f, 0f);
 			else if (Input.GetAxis("Horizontal") < -0.02f) transform.eulerAngles = new Vector3(0f, -90f, 0f);
@@ -31,17 +65,37 @@ public class CharacterMove : MonoBehaviour {
 			if (!animation.IsPlaying("jump")) PlayAnimation("run");
 		}
 		else CheckForIdle();
-	}
+	}*/
+
+	void  MoveHero (float h, float v){
+		if (Mathf.Abs (h) > 0.2f || Mathf.Abs (v) > 0.2f) {
+						if (Input.GetAxis ("Horizontal") < 0.02f)
+								transform.eulerAngles = new Vector3 (0f, 90f, 0f);
+						else if (Input.GetAxis ("Horizontal") > -0.02f)
+								transform.eulerAngles = new Vector3 (0f, -90f, 0f);
+
+						movement.Set (h, v, 0f);
+						movement = movement.normalized * speed * Time.deltaTime;
+						playerRigidbody.MovePosition (transform.position + movement);
+					//	rigidbody.AddForce( Physics.gravity );
+						if (!animation.IsPlaying ("jump"))	PlayAnimation ("run");
+
+				}
 
 	
+			//	CheckForIdle ();
+		}
+	
 	void  JumpHero (){
-		if (Input.GetButton("Jump") && nextJump < Time.time) {
-			rigidbody.AddForce(Vector3.up * 200);
+		/*if (Input.GetButton("Jump") && nextJump < Time.time) {
+			transform.Translate(Vector3.up * 50 * Time.deltaTime, Space.World);
+			//rigidbody.AddForce(Vector3.up * 200);
 			PlayAnimation("jump");
 			nextJump = Time.time + 1;
 			new WaitForSeconds(0.7f); 
-			PlayAnimation("idle");
-		}
+			PlayAnimation("idle");*/
+		//if (Input.GetKeyDown("space")) transform.Translate(Vector3.up * Mathf.Abs(Input.GetKeyDown("space")) * Time.deltaTime * 3.5f);
+
 	}
 	
 	void  OnCollisionEnter ( Collision collision  ){
