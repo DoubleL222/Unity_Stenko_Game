10bf18087e51a0d03c8430782a9dddf6b3cb667e
Luka Locniskar <luka.locniskar@gmail.com>
2015-01-11 19:50:18 +0100

    score

---
+++ TestProjectRgti/Assets/MyAssets/Scripts/displayPoints.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class displayPoints : MonoBehaviour {
+
+	public Text displayText;
+
+	private int points;
+	// Use this for initialization
+	void Start () {
+		points = PlayerPrefs.GetInt ("Level 1 returns");
+		if (points > 0 && points <= 10) {
+						displayText.text = "Preostale točke: "+points; 
+				} else if (points == 0) {
+						displayText.text = "Zmanjkalo je točk!"; 
+						PlayerPrefs.SetInt ("Level 1 returns", 10);
+				} else {
+					PlayerPrefs.SetInt ("Level 1 returns", 10);
+					points = PlayerPrefs.GetInt ("Level 1 returns");
+					displayText.text = "Preostale točke: "+points; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
